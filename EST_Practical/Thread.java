e6b067a4f893fd09e7fd75db3db8b20970ba06e9
Dushyant Singh Chauhan <162014027+CodeDushyant@users.noreply.github.com>
2025-11-17 20:31:32 +0530

    Update Thread.java

--- EST_Practical/Thread.java
+++ EST_Practical/Thread.java
@@ -1,1 +1,18 @@
+public class OddEvenThreads {
+    public static void main(String[] args) {
+        Printer p = new Printer();
 
+        Thread t1 = new Thread(() -> {
+            for (int i = 1; i <= 15; i += 2)
+                p.printOdd(i);
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 2; i <= 15; i += 2)
+                p.printEven(i);
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
